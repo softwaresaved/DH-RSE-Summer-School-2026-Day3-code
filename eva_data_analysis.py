71d1a7b3d30a334c7e88d46b628a8fcdb4f3384a
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2026-06-29 12:36:40 +0100

    Improve comments

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -7,25 +7,29 @@ f = 'eva_data.json'
 # TODO Inputs: this should be a command-line argument, not hardcoded
 o = 'eva_data.csv'
 
+# TODO: Unused function - candidate for removal
+fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
+
 print("--START--")
 
 print(f'Reading JSON data file {f}')
 # TODO Naming: 'd' is not descriptive
 d = pd.read_json(f, convert_dates=['date'], encoding='ascii')
-d['eva'] = d['eva'].astype(float)
-d.dropna(axis=0, subset=['duration', 'date'], inplace=True)
+d['eva'] = d['eva'].astype(float) # Not necessary, already read as float?
+d.dropna(axis=0, subset=['duration', 'date'], inplace=True) # drop rown where either duration or date in null
 
 print(f'Saving data to CSV file {o}')
 d.to_csv(o, index=False, encoding='utf-8')
 
 # TODO Descriptive comment: add an explanation of that the 3 lines below do
-subset = d.loc[:, ['crew', 'duration']]
-subset.crew = subset.crew.str.split(';').apply(lambda x: [i for i in x if i.strip()])
-subset = subset.explode('crew')
+subset = d.loc[:, ['crew', 'duration']] # subset of data with only columns crew and duration
+subset.crew = subset.crew.str.split(';').apply(lambda x: [i for i in x if i.strip()]) # anonymous function that takes a list of crew members and returns a list with whitespace stripped from names and empry stirngs removed
+subset = subset.explode('crew') # expand entries in a list-like column across multiple rows, making each element in the list a separate row and keeping/replicating values in other columns
 
-# TODO DRY: this duration-string-to-hours conversion is repeated again below
+# TODO DRY: duration-string-to-hours conversion is repeated again below
 # for the main dataframe - it should be a single reusable function
 hrs = []
+# Create a list of decimal values for duration in hours
 for val in subset['duration']:
     h, m = val.split(":")
     hrs.append(int(h) + int(m) / 60)
@@ -39,7 +43,7 @@ subset.to_csv(dur_out, index=False, encoding='utf-8')
 
 d.sort_values('date', inplace=True)
 
-# TODO DRY: duplicate of the hours-conversion logic above - violates DRY
+# TODO DRY: Duplicate of the hours-conversion logic above - violates DRY
 hrs2 = []
 for val in d['duration']:
     h, m = val.split(":")
@@ -48,10 +52,10 @@ d['duration_hours'] = hrs2
 
 d['cumulative_time'] = d['duration_hours'].cumsum()
 
-#TODO: import statements should be grouped at the top
+#TODO: Import statements should be grouped at the top
 import matplotlib.pyplot as plt
 
-# TODO Inputs: graph save location should be a flexible command-line argument
+# TODO Inputs: graph save location could be a flexible command-line argument
 g = 'cumulative_eva_graph.png'
 print(f'Plotting cumulative spacewalk duration and saving to {g}')
 plt.plot(d['date'], d['cumulative_time'], 'ko-')
@@ -61,10 +65,10 @@ plt.tight_layout()
 plt.savefig(g)
 plt.show()
 
-#TODO: import statements should be grouped at the top
+#TODO: Import statements should be grouped at the top
 import re
 
-# TODO: this function is never used anywhere - candidate for removal,
+# TODO: Unused function - candidate for removal
 # or for wiring into the analysis (left as unused/dead code on purpose)
 def calculate_crew_size(crew):
     if crew.split() == []:
